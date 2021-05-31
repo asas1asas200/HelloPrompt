b65717dca0664716d462d34471e29afa64b18c39
asas1asas200 <asas1asas200@gmail.com>
2021-05-31 15:45:33 +0800

    test(CalendarImageTest): test ASNI output of months

---
+++ src/test/java/CalendarImageTest.java
@@ -0,0 +1,18 @@
+package ntou.cs.java2021.helloprompt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CalendarImageTest {
+	@Test
+	public void showAllMonths(){
+		CalendarImage calendarImage = new CalendarImage();
+		for(int i=1;i<=12;i++){
+			System.out.println(calendarImage.getMonth(i));
+		}
+	}
+}
