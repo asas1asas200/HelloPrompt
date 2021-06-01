96ddc213e9120afb654c6a4d3a9e7eb981369562
asas1asas200 <asas1asas200@gmail.com>
2021-06-01 12:03:02 +0800

    test(CalendarImageTest): add weekdays display test

--- src/test/java/CalendarImageTest.java
+++ src/test/java/CalendarImageTest.java
@@ -30,4 +30,15 @@ public class CalendarImageTest {
 			System.out.println();
 		}
 	}
+
+	@Test
+	public void showAllDaysOfWeek(){
+		for(int i=1;i<=7;i++){
+			for(String line: calendarImage.getDayOfWeek(i)){
+				System.out.print(line);
+				System.out.println('#');
+			}
+			System.out.println();
+		}
+	}
 }
