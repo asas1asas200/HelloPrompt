472a834d70c9c36e738f650d0a4342a21fe97c02
asas1asas200 <asas1asas200@users.noreply.github.com>
2021-06-07 04:07:40 +0000

    deploy: 9fc3886405c8b4a1fbdef7878a4566c7270cae13

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"snippets","l":"ANSIColor"},{"p":"handler","l":"CalendarHandler"},{"p":"snippets","l":"CalendarImage"},{"p":"<Unnamed>","l":"CLI"},{"p":"handler","l":"Handler"},{"p":"<Unnamed>","l":"HelloPrompt"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"snippets","l":"ANSIColor"},{"p":"handler","l":"CalendarHandler"},{"p":"snippets","l":"CalendarImage"},{"p":"<Unnamed>","l":"CLI"},{"p":"handler","l":"Handler"},{"p":"<Unnamed>","l":"HelloPrompt"},{"p":"handler","l":"WeatherHandler"}]
