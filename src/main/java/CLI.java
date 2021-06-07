6c60d1d2acc7107a6800a7e8058f2b76c8c4ae1f
asas1asas200 <asas1asas200@gmail.com>
2021-06-07 11:32:32 +0800

    docs(CLI): insert javadoc comment into CLI

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -3,14 +3,25 @@ import java.util.List;
 
 import handler.*;
 
+/**
+ * CLI is a interface for calling all handlers and display prompt on screen.
+ *
+ * @author <a href="mailto:asas1asas200@gmaill.com">Zeng</a>
+ */
 public class CLI {
 	private List<Handler> handlers;
 
+	/**
+	 * This contructor defined the handlers that will run.
+	 */
 	public CLI() {
 		handlers = new ArrayList<Handler>();
 		handlers.add(new CalendarHandler());
 	}
 
+	/**
+	 * Call this function to run all handlers and prompt it!
+	 */
 	public void run() {
 		for (Handler handler : handlers) {
 			if (handler.ifOutput) {
