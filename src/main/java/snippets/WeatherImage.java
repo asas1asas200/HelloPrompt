4406d4b46fd80ebd74c2d30f9511e6baf99bf534
asas1asas200 <asas1asas200@gmail.com>
2021-06-23 02:10:50 +0800

    docs(WeatherImage): insert JavaDoc comments into class

--- src/main/java/snippets/WeatherImage.java
+++ src/main/java/snippets/WeatherImage.java
@@ -4,6 +4,11 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+/**
+ * This class contains ascii image that {@code WeatherHandler} will display.
+ * 
+ * @author <a href="mailto:asas1asas200@gmail.com">Zeng</a>
+ */
 public class WeatherImage {
 //@formatter:off
 	private final static List<String> UNKNOWN = Arrays.asList(
@@ -82,20 +87,44 @@ public class WeatherImage {
 " |" + ANSIColor.RED_BACKGROUND + " " + ANSIColor.RESET + "|         ",
 " |" + ANSIColor.RED_BACKGROUND + " " + ANSIColor.RESET + "|         ",
 " (" + ANSIColor.RED_BACKGROUND + ANSIColor.PURPLE + "_" + ANSIColor.RESET + ")         ");
-
-	public final static String WIND_DIR = "↑↗→↘↓↙←↖";
-
 //@formatter:on
 
+	private final static String WIND_DIR = "↑↗→↘↓↙←↖";
+
+	/**
+	 * Passing a wind direction between 0~360 and that will return a symbol of wind
+	 * direction.
+	 * 
+	 * @param dir a number indicates wind directions between 0 ~ 360.
+	 * @return {@code char} symbol of wind direction.
+	 */
 	public static char getWindDir(int dir) {
 		return WIND_DIR.charAt(dir / 45 % 8);
 	}
 
+	/**
+	 * Another weather parser.
+	 * 
+	 * @see #getWeatherImage(String)
+	 * 
+	 * @param weatherDescs a list of weather description.
+	 * @return {@code List<String>} weather image.
+	 */
 	public static List<String> getWeatherImage(List<String> weatherDescs) {
 		// Because only index 0 of list contains the information we need.
 		return getWeatherImage(weatherDescs.get(0));
 	}
 
+	/**
+	 * Render thermometer image by passing high and low temp.
+	 * 
+	 * @param high           heighest temp.
+	 * @param low            lowest temp.
+	 * @param lastLineOffset {@code false} the last line of image will not contain
+	 *                       spaces.
+	 * @return {@code List<String>} A thermometer image include highest and lowest
+	 *         temp prompts.
+	 */
 	public static List<String> getTempImage(String high, String low, Boolean lastLineOffset) {
 		List<String> temp = getTempImage(high, low);
 		if (!lastLineOffset)
@@ -103,6 +132,14 @@ public class WeatherImage {
 		return temp;
 	}
 
+	/**
+	 * Render thermometer image by passing high and low temp.
+	 * 
+	 * @param high heighest temp.
+	 * @param low  lowest temp.
+	 * @return {@code List<String>} A thermometer image include highest and lowest
+	 *         temp prompts.
+	 */
 	public static List<String> getTempImage(String high, String low) {
 		List<String> temp = new ArrayList<String>(TEMP);
 		temp.set(1, String.format(temp.get(1), high));
@@ -110,6 +147,14 @@ public class WeatherImage {
 		return temp;
 	}
 
+	/**
+	 * Render thermometer image by passing current temp.
+	 * 
+	 * @param temp           current temp.
+	 * @param lastLineOffset {@code false} the last line of image will not contain
+	 *                       spaces.
+	 * @return {@code List<String>} A thermometer image include current temp.
+	 */
 	public static List<String> getTempImage(String temp, Boolean lastLineOffset) {
 		List<String> tempImg = getTempImage(temp);
 		if (!lastLineOffset)
@@ -117,12 +162,25 @@ public class WeatherImage {
 		return tempImg;
 	}
 
+	/**
+	 * Render thermometer image by passing current temp.
+	 * 
+	 * @param temp current temp.
+	 * @return {@code List<String>} A thermometer image include current temp.
+	 */
 	public static List<String> getTempImage(String temp) {
 		List<String> tempImg = new ArrayList<String>(TEMP_TODAY);
 		tempImg.set(2, String.format(tempImg.get(2), temp.substring(0, temp.length() < 3 ? temp.length() : 3)));
 		return tempImg;
 	}
 
+	/**
+	 * Parse current weather description string to an image.
+	 *
+	 * @param weatherDesc a description of current weather.
+	 * @return {@code List<String>} if matched, it will return the matching image,
+	 *         otherwise return the UNKNOWN image.
+	 */
 	public static List<String> getWeatherImage(String weatherDesc) {
 		if (weatherDesc.contains("陰") || weatherDesc.contains("雲")) {
 			if (weatherDesc.contains("晴")) {
@@ -155,6 +213,13 @@ public class WeatherImage {
 		return UNKNOWN;
 	}
 
+	/**
+	 * To compare if the image is equals with UNKNOWN image.
+	 * 
+	 * @param ANSIImage the image will be compared.
+	 * @return {@code true} the image is equal to UNKNOWN.<br>
+	 *         {@code false} the image isn't equal to UNKNOWN.
+	 */
 	public static Boolean isUnknown(List<String> ANSIImage) {
 		return ANSIImage.equals(UNKNOWN);
 	}
