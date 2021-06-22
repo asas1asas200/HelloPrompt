12273eee2a3a34cdd8e346aa5de33ba3697b6dba
asas1asas200 <asas1asas200@gmail.com>
2021-06-23 02:16:53 +0800

    docs(GitImage): insert JavaDoc comments

--- src/main/java/snippets/GitImage.java
+++ src/main/java/snippets/GitImage.java
@@ -3,6 +3,11 @@ package snippets;
 import java.util.List;
 import java.util.Arrays;
 
+/**
+ * This class generate the ascii image about {@code GitHandler}.
+ * 
+ * @author <a href="mailto:asas1asas200@gmail.com">Zeng</a>
+ */
 public class GitImage {
 //@formatter:off
 	private final static String ORANGE = "\033[38:2:255:165:0m"; // It's not 3/4 standard, hence not in ANSIColor class.
@@ -25,10 +30,20 @@ ANSIColor.BLUE + " __   ___  __   __   " + ANSIColor.YELLOW + "        ___  __  
 ANSIColor.BLUE + "|__) |__  |__) /  \\ " + ANSIColor.YELLOW + "   |__| |__  |__) |__   " + ANSIColor.RESET,
 ANSIColor.BLUE + "|  \\ |___ |    \\__/" + ANSIColor.YELLOW + "    |  | |___ |  \\ |___ ." + ANSIColor.RESET);
 //@formatter:on
+	/**
+	 * Get the GitLogo.
+	 * 
+	 * @return {@code List<String>} Git Logo.
+	 */
 	public static List<String> getLogo() {
 		return GITLOGO;
 	}
 
+	/**
+	 * Get no repo prompt image.
+	 * 
+	 * @return {@code List<String>} No repo image.
+	 */
 	public static List<String> getNOREPO() {
 		return NOREPO;
 	}
